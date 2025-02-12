89ad99166a239c1cb12a0fa57982511b90f3508c
sunnymodi29 <modisunny25@gmail.com>
2025-02-12 15:23:25 +0530

    Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,12 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  //   <Provider store={reduxStore}>
+  //     <App />
+  //   </Provider>
+  // </React.StrictMode>
   <React.StrictMode>
-    <Provider store={reduxStore}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
