8f4975901e7802908c04e9e977a41b9657dadf85
sunnymodi29 <modisunny25@gmail.com>
2025-02-12 15:29:51 +0530

    Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import reduxStore from "../redux/reduxStore.js";
+// import { Provider } from "react-redux";
+// import reduxStore from "../redux/reduxStore.js";
 import App from "./App.jsx";
 import "./index.css";
 
